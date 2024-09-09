bb3ca2763bcedcc1bbca8e8c646539bb2c05234a
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-09 20:19:15 +0000

    Adds loader for sass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as _ from "lodash";
+import "./style.sass";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
 }
