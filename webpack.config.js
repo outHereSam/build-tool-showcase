70eb11e5a328d0c474dd3849a0893b5e327e586c
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-10 08:34:52 +0000

    Adds babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,43 @@
-const path = require("path");
+import { resolve } from "path";
+// import path from 'path'
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+export const entry = "./src/index.js";
+export const output = {
+  filename: "bundle.js",
+  // eslint-disable-next-line no-undef
+  path: resolve(__dirname, "dist"),
+};
+export const module = {
+  rules: [
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: "asset/resource",
+    },
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", { targets: "defaults" }]],
+        },
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  mode: "development",
-  devtool: "source-map",
-  devServer: {
-    static: "./dist",
-  },
+    },
+  ],
+};
+export const mode = "development";
+export const devtool = "source-map";
+export const devServer = {
+  static: "./dist",
 };
