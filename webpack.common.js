a4acae4a7eca2edf693b3869cbdd70b380f26b7d
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-10 10:04:46 +0000

    Configures production build

--- webpack.common.js
+++ webpack.common.js
@@ -1,43 +1,38 @@
-import { resolve } from "path";
-// import path from 'path'
+/* eslint-disable no-undef */
+// Import the necessary modules using CommonJS `require`
+// eslint-disable-next-line no-undef
+const path = require("path");
 
-export const entry = "./src/index.js";
-export const output = {
-  filename: "bundle.js",
-  // eslint-disable-next-line no-undef
-  path: resolve(__dirname, "dist"),
-};
-export const module = {
-  rules: [
-    {
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader",
-        // Compiles Sass to CSS
-        "sass-loader",
-      ],
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: "asset/resource",
-    },
-    {
-      test: /\.(?:js|mjs|cjs)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: [["@babel/preset-env", { targets: "defaults" }]],
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]], // Transpile modern JS
+          },
         },
       },
-    },
-  ],
-};
-export const mode = "production";
-export const devtool = "source-map";
-export const devServer = {
-  static: "./dist",
+    ],
+  },
+  devtool: "source-map", // Generate source maps for debugging
+  devServer: {
+    static: "./dist", // Serve files from the dist directory
+  },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,5 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
+  mode: "production",
 });
